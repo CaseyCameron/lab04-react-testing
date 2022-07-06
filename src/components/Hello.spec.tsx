9beffd76d97987803e974af976401f5801e43fd9
CaseyCameron <mcasey.cameron@gmail.com>
2022-07-06 10:36:39 -0700

    Added Counter

--- src/components/Hello.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Hello } from './Hello';
-
-it('renders hello world', () => {
-  render(<Hello />);
-  const myElement = screen.getByText(/hello world!/i);
-  expect(myElement).toBeInTheDocument();
-});
