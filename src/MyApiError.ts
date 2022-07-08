6c9b8cc33e3464317b08964459f2f2dbdbfde5e6
CaseyCameron <mcasey.cameron@gmail.com>
2022-07-08 13:48:31 -0700

    forms testing

--- src/MyApiError.ts
+++
@@ -1,3 +0,0 @@
-export interface MyApiError {
-  message: string;
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-// import { Counter } from "../components/Counter";
-import { PhotosList } from "../components/PhotosList";
+import { MultiStepForm } from "../components/MultiStepForm";
 
 export default function Home() {
-  return <PhotosList />;
-  // return (
-  //   <div>
-  //     <h1>Hello React.js Testing Series Friends!!!</h1>
-  //     <Counter description="My Counter" defaultCount={0} />
-  //   </div>
-  // );
+  return (
+    <MultiStepForm
+      onSubmit={(values) => {
+        console.log("Form Submitted", values);
+      }}
+    />
+  );
 }
